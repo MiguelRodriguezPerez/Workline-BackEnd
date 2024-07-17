4beefa30772152c285367e1fbc4154af05cf6305
Miguel Rodríguez <jazmindemar33@gmail.com>
2024-07-17 20:34:59 +0200

    indexOfertas responsive

--- demo/src/main/resources/static/js/buscarTrabajo/indexBuscarTrabajo.js
+++ demo/src/main/resources/static/js/buscarTrabajo/indexBuscarTrabajo.js
@@ -2,13 +2,17 @@
 
 /* Set the width of the side navigation to 250px */
 function openNav() {
-    document.getElementById("mySidenav").style.width = "300px";
+    if(window.innerWidth > 700) document.getElementById("mySidenav").style.width = "300px";
+    else document.getElementById("mySidenav").style.width = "100%";
+    
   }
   
 /* Set the width of the side navigation to 0 */
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
 }
+
+document.getElementById('buttonNav').addEventListener('click',openNav);
 
 document.addEventListener('click',(e)=>{
   if(e.layerX > 300) closeNav();
