f87818952a5e8e18afea011e0443ca3eb44f5403
Miguel Rodríguez <jazmindemar33@gmail.com>
2024-07-17 17:19:23 +0200

    ofertas menu desplegable sin refactorizar

---
+++ demo/src/main/resources/static/js/buscarTrabajo/indexBuscarTrabajo.js
@@ -0,0 +1,15 @@
+"use strict"
+
+/* Set the width of the side navigation to 250px */
+function openNav() {
+    document.getElementById("mySidenav").style.width = "300px";
+  }
+  
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
+
+document.addEventListener('click',(e)=>{
+  if(e.layerX > 300) closeNav();
+})
