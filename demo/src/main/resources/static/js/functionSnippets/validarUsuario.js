dcf564ea421e01532e8bc6e2f5160ca31d7ce9fe
Miguel Rodríguez <jazmindemar33@gmail.com>
2024-08-01 20:39:27 +0200

    validación "correcta" pendiente submit

--- demo/src/main/resources/static/js/functionSnippets/validarUsuario.js
+++ demo/src/main/resources/static/js/functionSnippets/validarUsuario.js
@@ -1,89 +1,41 @@
-export function subrayarError(arg){
-    arg.classList.add('inputError');
+export function mostrarError(fallo,input){
+    fallo.style.display = 'block';
+    input.classList.add('inputError');
 }
 
-export function limpiarSubrayadoError(arg){
-    arg.classList.remove('inputError');
+export function limpiarError(fallo,input){
+    fallo.style.display = 'none';
+    input.classList.remove('inputError');
 }
 
-export async function validarNombreUsuario(inputTag,error1,error2){
-    const valorInput = inputTag.value;
-    
-    if(valorInput > 25 || valorInput === ''){
-        inputTag.classList.add('inputError');
-        error1.style.display = 'block';
-        return false;
-    } 
-    /*La petición a la api se realiza después de comprobar que el nombre no esta vacío, porque
-    si no fallaría al enviar un nombre vacío */
-    else{
-        inputTag.classList.remove('inputError');
-        error1.style.display = 'none';
-
-        const peticionNombreRepetido = await fetch('/nuevoUsuarioCreacion/esRepetido/' + valorInput);
-        const esNombreRepetido = await peticionNombreRepetido.json();
-        console.log(esNombreRepetido);
-
-        if(esNombreRepetido === true){
-            inputTag.classList.add('inputError');
-            error2.style.display = 'block';
-            return false;
-        } 
-        else{
-            
-            inputTag.classList.remove('inputError');
-            error2.style.display = 'none';
-            return true;
-        } 
-    }
-
+export function validarNombreUsuario(nombre) {
+    return (nombre.length < 25 && nombre !== '');
 }
 
-export function validarEmail(inputTag,error,error2){   
-    if(inputTag.value.length > 35){
-        inputTag.classList.add('inputError');
-        error.style.display = 'block';
-        return false;
-    } 
-    else if(!/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(inputTag.value)){
-        inputTag.classList.add('inputError');
-        error2.style.display = 'block';
-        return false;
-    }
-    else{
-        inputTag.classList.remove('inputError');
-        error.style.display = 'none';
-        error2.style.display = 'none';
-        return true;
-    }
+export async function esNombreRepetido(nombre) {
+    const peticionNombreRepetido = await fetch('/nuevoUsuarioCreacion/esRepetido/' + nombre);
+    const esNombreRepetido = await peticionNombreRepetido.json();
+    console.log(esNombreRepetido);
+    return esNombreRepetido;
 }
 
-export function validarPassword(inputTag,error){
-    if(inputTag.value.length > 30 || inputTag.value.length < 14 || !/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\w\s]).*$/.test(inputTag.value)){
-        inputTag.classList.add('inputError');
-        error.style.display = 'block';
-        return false;
-    }
-    else{
-        inputTag.classList.remove('inputError');
-        error.style.display = 'none';
-        return true;
-    }
-}
-export function validarTelefono(inputTag,error){
-    if (!/^(6|7|8|9)\d{8}$/.test(inputTag)){
-        inputTag.classList.add('inputError');
-        error.style.display = 'block';
-        return false;
-    }
-    else{
-        inputTag.classList.remove('inputError');
-        error.style.display = 'none';
-        return true;
-    }
+export function validarLongitudEmail(email) {
+    return (email.length < 35);
 }
 
+export function validarExpresionEmail(email) {
+    return (/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(email));
+}
 
-    
+export function validarPassword(password) {
+    return (password.length < 30 && password.length > 14 
+    && /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\w\s]).*$/.test(password));
+}
 
+export function validarTelefono(telefono) {
+    return (/^(6|7|8|9)\d{8}$/.test(telefono));
+}
 
+export function validarCiudad(ciudad) {
+    return (ciudad !== '');
+}
--- demo/src/main/resources/static/js/usuarioPerfil/editarDatosPerfil.js
+++ demo/src/main/resources/static/js/usuarioPerfil/editarDatosPerfil.js
@@ -4,20 +4,83 @@ document.querySelectorAll('#formularioDatos input:not([hidden]):not([type="submi
 
 const arrayInputs = document.querySelectorAll('#formularioDatos input:not([hidden]):not([type="submit"])');
 const arrayFallos = document.querySelectorAll('.mensajeError');
-console.log(arrayFallos);
+const formulario = document.getElementById('formularioDatos');
 
-arrayInputs[0].addEventListener('input',function(){
-    funciones.validarNombreUsuario(this,arrayFallos[0],arrayFallos[1]);
+const valNombre = async () =>{
+    if(!funciones.validarNombreUsuario(arrayInputs[0].value)){
+        funciones.mostrarError(arrayFallos[0],arrayInputs[0]);
+        return false;
+    }
+    else if(await funciones.esNombreRepetido(arrayInputs[0].value) === true){
+        funciones.limpiarError(arrayFallos[0],arrayInputs[0]);
+        funciones.mostrarError(arrayFallos[1],arrayInputs[0]);
+        return false;
+    }
+    else{
+        funciones.limpiarError(arrayFallos[0],arrayInputs[0]);
+        funciones.limpiarError(arrayFallos[1],arrayInputs[0]);
+        return true;
+    }
+}
+arrayInputs[0].addEventListener('input',valNombre);
+
+const valEmail = () =>{
+    if(!funciones.validarLongitudEmail(arrayInputs[1].value)){
+        funciones.mostrarError(arrayFallos[2],arrayInputs[1]);
+        return false;
+    }
+    else if(!funciones.validarExpresionEmail(arrayInputs[1].value)){
+        funciones.limpiarError(arrayFallos[2],arrayInputs[1]);
+        funciones.mostrarError(arrayFallos[3],arrayInputs[1]);
+        return false;
+    }
+    else{
+        funciones.limpiarError(arrayFallos[2],arrayInputs[1]);
+        funciones.limpiarError(arrayFallos[3],arrayInputs[1]);
+        return true;
+    }
+}
+arrayInputs[1].addEventListener('input',valEmail);
+
+const valPassword = () =>{
+    if(!funciones.validarPassword(arrayInputs[2].value)){
+        funciones.mostrarError(arrayFallos[4],arrayInputs[2]);
+        return false;
+    }
+    else{
+        funciones.limpiarError(arrayFallos[4],arrayInputs[2]);
+        return true;
+    }
+}
+arrayInputs[2].addEventListener('input',valPassword);
+
+const valTelefono = () =>{
+    if(!funciones.validarTelefono(arrayInputs[3].value)){
+        funciones.mostrarError(arrayFallos[5],arrayInputs[3]);
+        return false;
+    }
+    else{
+        funciones.limpiarError(arrayFallos[5],arrayInputs[3]);
+        return true;
+    }
+}
+arrayInputs[3].addEventListener('input',valTelefono);
+
+const valCiudad = () =>{
+    if(!funciones.validarCiudad(arrayInputs[4].value)){
+        funciones.mostrarError(arrayFallos[6],arrayInputs[4]);
+        return false;
+    }
+    else{
+        funciones.limpiarError(arrayFallos[6],arrayInputs[4]);
+        return true;
+    }
+}
+arrayInputs[4].addEventListener('input',valCiudad);
+
+
+document.getElementById('editarUsuario').addEventListener('click', async (e) => {
+    e.preventDefault();
+    const nombreValido = await valNombre(); // Garantiza que esta promesa se resuelva
+    if(nombreValido && valEmail() && valPassword() && valTelefono() && valCiudad()) formulario.submit();
 });
-
-arrayInputs[1].addEventListener('input',function(){
-    funciones.validarEmail(this,arrayFallos[2],arrayFallos[3]);
-});
-
-arrayInputs[2].addEventListener('input',function(){
-    funciones.validarPassword(this,arrayFallos[4]);
-});
-
-arrayInputs[3].addEventListener('input',function(){
-    funciones.validarTelefono(this,arrayFallos[5]);
-})
