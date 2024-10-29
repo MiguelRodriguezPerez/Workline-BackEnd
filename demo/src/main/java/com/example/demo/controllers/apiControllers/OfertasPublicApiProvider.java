7d92d1f4374bff6a51285f4b4cd23c09ddb148c7
Miguel Rodríguez <jazmindemar33@gmail.com>
2024-10-29 21:19:51 +0100

    ofertaFindById  api

--- demo/src/main/java/com/example/demo/controllers/apiControllers/OfertasPublicApiProvider.java
+++ demo/src/main/java/com/example/demo/controllers/apiControllers/OfertasPublicApiProvider.java
@@ -8,6 +8,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -47,6 +48,13 @@ public class OfertasPublicApiProvider {
     @GetMapping("/tiposContrato")
     public ResponseEntity<List<TipoContrato>> getAllTiposContrato(){
         List<TipoContrato> resultado = Arrays.asList(TipoContrato.values());
+        return new ResponseEntity<>(resultado, HttpStatus.OK);
+    }
+
+    @GetMapping("/getOfertaById/{id}")
+    public ResponseEntity<Oferta> getOfertaByIdApi(@PathVariable Long id){
+        Oferta resultado = ofertaService.obtenerPorId(id);
+        if(resultado == null) return new ResponseEntity<>(HttpStatus.NOT_FOUND); 
         return new ResponseEntity<>(resultado, HttpStatus.OK);
     }
 
