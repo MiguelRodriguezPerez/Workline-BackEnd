f6f5aa6ab9ccf1a5d368d81ce92424d9e51d93c0
Miguel Rodríguez <jazmindemar33@gmail.com>
2024-11-02 20:02:01 +0100

    funciones login logout

--- demo/src/main/java/com/example/demo/config/UsuarioService.java
+++ demo/src/main/java/com/example/demo/config/UsuarioService.java
@@ -1,13 +1,19 @@
 package com.example.demo.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import com.example.demo.domain.entidadesApi.UserLoginRequest;
 import com.example.demo.domain.usuarios.Admin;
 import com.example.demo.domain.usuarios.Busca;
 import com.example.demo.domain.usuarios.Contrata;
@@ -16,6 +22,8 @@ import com.example.demo.domain.usuarios.Usuario;
 import com.example.demo.services.usuarios.AdminService;
 import com.example.demo.services.usuarios.BuscaService;
 import com.example.demo.services.usuarios.ContrataService;
+
+import jakarta.annotation.Resource;
 
 @Service
 public class UsuarioService {
@@ -26,11 +34,14 @@ public class UsuarioService {
     @Autowired
     BuscaService buscaService;
 
-    @Autowired 
+    @Autowired
     AdminService adminService;
 
     @Autowired
     PasswordEncoder encoder;
+
+    @Autowired
+    private AuthenticationManager authenticationManager; 
 
     public boolean esNombreRepetido(String nombre){
         /*El primer if sirve para evitar que el usuario conectado no pueda actualizar sus datos
@@ -50,6 +61,35 @@ public class UsuarioService {
         if(buscaService.obtenerBuscaConectado() != null) return (Busca) buscaService.obtenerBuscaConectado();
         if(adminService.obtenerAdminConectado() != null) return (Admin) adminService.obtenerAdminConectado();
         return null;
+    }
+
+    public Usuario loginUsuario(UserLoginRequest userLoginRequest){
+
+        try {
+            Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(userLoginRequest.getUsername(), userLoginRequest.getPassword()));
+            if(authentication.isAuthenticated()) {
+                System.out.println("BIIIIIIIEEEEEEN");
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+                Usuario usuario = this.obtenerUsuarioConectado();
+                return usuario;
+            }
+            
+        } 
+        catch (AuthenticationException e) {
+            return null;
+        }
+        return null;
+    }
+
+    public boolean logoutUsuario(){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if(authentication != null){
+            System.out.println("YASSSSSSSSSSSSSSSSSSSSSSSSSSSSS");
+            SecurityContextHolder.clearContext();
+            return true;
+        }
+        return false;
     }
 
 }
--- demo/src/main/java/com/example/demo/controllers/LoginApiController.java
+++ demo/src/main/java/com/example/demo/controllers/LoginApiController.java
@@ -4,12 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -22,51 +18,24 @@ import com.example.demo.domain.usuarios.Usuario;
 
 import jakarta.annotation.Resource;
 
-
 @RequestMapping("/api/logins")
 @RestController
 public class LoginApiController {
-
-    @Resource(name = "authenticationManager")
-    private AuthenticationManager authenticationManager; 
-
-    @Autowired
-    PasswordEncoder encoder;
 
     @Autowired
     UsuarioService usuarioService;
     
     @PostMapping("/login")
     public ResponseEntity<Usuario> postMethodName(@RequestBody UserLoginRequest user) {
-        
-        try {
-            Authentication authentication = authenticationManager.authenticate(
-                new UsernamePasswordAuthenticationToken(user.getUsername(), user.getPassword()));
-            if(authentication.isAuthenticated()) {
-                System.out.println("BIIIIIIIEEEEEEN");
-                SecurityContextHolder.getContext().setAuthentication(authentication);
-                Usuario usuario = usuarioService.obtenerUsuarioConectado();
-                return new ResponseEntity<>(usuario, HttpStatus.OK);
-            }
-                
-            } 
-            catch (AuthenticationException e) {
-                return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
-            }
-
-            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
-    }
+        Usuario usuario = usuarioService.loginUsuario(user);
+        if(usuario == null) return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+        return new ResponseEntity<>(usuario,HttpStatus.OK);
+    } 
 
     @GetMapping("/logout")
     public ResponseEntity<?> postLogout() {
-          Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-            if(authentication != null){
-                System.out.println("YASSSSSSSSSSSSSSSSSSSSSSSSSSSSS");
-                SecurityContextHolder.clearContext();
-                return new ResponseEntity<>(HttpStatus.OK); 
-            }
-            
-        return new ResponseEntity<>(HttpStatus.BAD_REQUEST); 
+        if(usuarioService.logoutUsuario()) return new ResponseEntity<>(HttpStatus.OK);
+        else return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
     }
     
 
