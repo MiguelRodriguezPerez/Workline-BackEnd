ce2ff30cc33288c9d4fdb3cc5e47caa8dce7b1e2
Miguel Rodríguez <jazmindemar33@gmail.com>
2024-11-04 18:56:21 +0100

    loggedUserEndpoint

--- demo/src/main/java/com/example/demo/controllers/LoginApiController.java
+++ demo/src/main/java/com/example/demo/controllers/LoginApiController.java
@@ -3,9 +3,6 @@ package com.example.demo.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,8 +12,6 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.demo.config.UsuarioService;
 import com.example.demo.domain.entidadesApi.UserLoginRequest;
 import com.example.demo.domain.usuarios.Usuario;
-
-import jakarta.annotation.Resource;
 
 @RequestMapping("/api/logins")
 @RestController
@@ -38,5 +33,11 @@ public class LoginApiController {
         else return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
     }
     
-
+    @GetMapping("/currentUser")
+    public ResponseEntity<Usuario> getCurrentUser(){
+        Usuario usuario = usuarioService.obtenerUsuarioConectado();
+        //Es una mala idea, pero evita errores en la consola del navegador
+        if(usuario == null) return new ResponseEntity<>(null,HttpStatus.OK);
+        else return new ResponseEntity<>(usuario,HttpStatus.OK);
+    }
 }
